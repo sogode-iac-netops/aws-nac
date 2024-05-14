c2231f4f1df19786f28e2dbd9a044ceb41413a8d
Peet van de Sande <peet@peetvandesande.com>
2024-05-14 12:03:24 +0200

    Added tgw_arn as output

--- modules/transit_gateway/outputs.tf
+++ modules/transit_gateway/outputs.tf
@@ -4,6 +4,10 @@ output "tgw_id" {
   value = aws_ec2_transit_gateway.tgw.id
 }
 
+output "tgw_arn" {
+  value = aws_ec2_transit_gateway.tgw.arn
+}
+
 output "tgw_association_default_route_table_id" {
   value = aws_ec2_transit_gateway.tgw.association_default_route_table_id
 }

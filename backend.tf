02f43baa83365f7204bdd0b1602aa8057432911f
sogode-iac-root <legrand@iac.sogode.com>
2024-05-31 15:59:28 +0200

    Update backend.tf
    Removed comments

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
 terraform {
 #  required_version = ">= 1.8.0"
 
-#   # Use remote backend for HCP Terraform (formerly Terraform Cloud)
-#   backend "remote" {
-#     hostname = "app.terraform.io"
-#     organization = "<orgid>"
-#     workspaces {
-#         name = "aws-nac" # Verify!
-#     }
-#   }
+  # Use remote backend for HCP Terraform (formerly Terraform Cloud)
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "<orgid>"
+    workspaces {
+        name = "aws-nac" # Verify!
+    }
+  }
 }

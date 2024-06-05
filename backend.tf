af64865c05ab083d77fdd9ccef9db0c63faa699e
Sogode IaC NetOps role <netops@iac.sogode.com>
2024-06-05 10:28:57 +0200

    Updated org ID

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
   # Use remote backend for HCP Terraform (formerly Terraform Cloud)
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "b79f2c4e-652c-4465-8e61-b7a1c7b72de3"
+    organization = "org-Sbm4Md8AimaXpfub"
     workspaces {
         name = "aws-nac" # Verify!
     }
